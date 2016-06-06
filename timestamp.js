6251ebb01c1884e3dfe565bbcd67066f67a2040c
Andrew Boyle <ajboyle31@gmail.com>
2016-06-06 00:30:49 +0000

    completed app, need to test and debug

--- timestamp.js
+++ timestamp.js
@@ -19,10 +19,54 @@ app.get('/', function(req, res){
     res.render('main');
 });
 
+var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var year, monthNumber, month, day, unix;        
 
 app.get('/:date', function(req, res){
     
-    res.end('I have received ' + req.params.date);
+
+    
+    function isNatural(enteredDate) {
+        year = enteredDate.getFullYear();
+        monthNumber = enteredDate.getMonth();
+        month = monthNames[monthNumber];
+        day = enteredDate.getDate();
+        unix = enteredDate.getTime();
+        
+        return '{"unix":' + unix.toString().slice(0,-3) + ',"natural":' + '"' + month + ' ' + day + ', ' + year + '"}';
+    }
+    
+    function isUnix(enteredDate) {
+        year = enteredDate.getFullYear();
+        monthNumber = enteredDate.getMonth();
+        month = monthNames[monthNumber];
+        day = enteredDate.getDate();
+        unix = enteredDate.getTime();
+        
+        return '{"unix":' + unix.toString().slice(0,-3) + ',"natural":' + '"' + month + ' ' + day + ', ' + year + '"}';
+    }
+    
+    
+    var enteredDateString = new Date(req.params.date);
+    var enteredDateNumber = new Date(Number(req.params.date) * 1000);
+    
+    if (enteredDateString == 'Invalid Date'){
+        if (enteredDateNumber == 'Invalid Date'){
+            res.end('{"unix":null,"natural":null}');
+        }
+        else {
+            res.end(isUnix(enteredDateNumber));
+        }
+    }
+    else {
+        res.end(isNatural(enteredDateString));
+    }
+    
+    
+    
+    
+    
+
 });
 
 
